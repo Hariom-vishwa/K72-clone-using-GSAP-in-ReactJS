d8e0858d1be63abd9658ed8497f5286c1d7ece06
Hariom-vishwa <hawkgamerhv@gmail.com>
2025-11-07 16:51:22 +0530

    project card animation

--- src/components/Projects/ProjectsCard.jsx
+++ src/components/Projects/ProjectsCard.jsx
@@ -1,18 +1,31 @@
-const ProjectsCard = () => {
+
+
+const ProjectsCard = (props) => {
+
   return (
     <>
-      <div className="w-full h-[644px] flex gap-4 mb-4 ">
-        <div className="w-1/2 h-full group transition-all relative overflow-hidden hover:rounded-4xl">
-          <img
-            className="w-full h-full group-hover:scale-[1.05] transition-all object-cover"
-            src="../images/projects-img-1.jpg"
-            alt="img-1"
-          />
-          <div className="absolute group-hover:opacity-100 opacity-0 top-0 flex items-center justify-center left-0 h-full w-full bg-black/20">
-            <h2 className="uppercase text-6xl font-[font1] border-4 pt-4 px-4 text-white border-white rounded-full">
-              Voir le projet
-            </h2>
-          </div>
+      <div className="w-1/2 h-full group transition-all relative overflow-hidden hover:rounded-4xl">
+        <img
+          className="w-full h-full group-hover:scale-[1.05] transition-all object-cover"
+          src={props.image1}
+          alt="img-1"
+        />
+        <div className="absolute group-hover:opacity-100 opacity-0 top-0 flex items-center justify-center left-0 h-full w-full bg-black/20">
+          <h2 className="uppercase text-6xl font-[font1] border-4 pt-4 px-4 text-white border-white rounded-full">
+            Voir le projet
+          </h2>
+        </div>
+      </div>
+      <div className="w-1/2 h-full group transition-all relative overflow-hidden hover:rounded-4xl">
+        <img
+          className="w-full h-full group-hover:scale-[1.05] transition-all object-cover"
+          src={props.image2}
+          alt="img-1"
+        />
+        <div className="absolute group-hover:opacity-100 opacity-0 top-0 flex items-center justify-center left-0 h-full w-full bg-black/20">
+          <h2 className="uppercase text-6xl font-[font1] border-4 pt-4 px-4 text-white border-white rounded-full">
+            Voir le projet
+          </h2>
         </div>
       </div>
     </>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,77 @@
+import gsap from "gsap";
 import ProjectsCard from "../components/Projects/ProjectsCard";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 
 const Projects = () => {
+  const images = [
+    {
+      img1: "../images/projects-img-13.jpg",
+      img2: "../images/projects-img-14.jpg",
+    },
+    {
+      img1: "../images/projects-img-1.jpg",
+      img2: "../images/projects-img-2.jpg",
+    },
+    {
+      img1: "../images/projects-img-3.jpg",
+      img2: "../images/projects-img-4.jpg",
+    },
+    {
+      img1: "../images/projects-img-5.jpg",
+      img2: "../images/projects-img-6.jpg",
+    },
+    {
+      img1: "../images/projects-img-7.jpg",
+      img2: "../images/projects-img-8.jpg",
+    },
+    {
+      img1: "../images/projects-img-9.jpg",
+      img2: "../images/projects-img-10.jpg",
+    },
+    {
+      img1: "../images/projects-img-11.jpg",
+      img2: "../images/projects-img-12.jpg",
+    },
+    {
+      img1: "../images/projects-img-13.jpg",
+      img2: "../images/projects-img-14.jpg",
+    },
+  ];
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(function () {
+    gsap.from(".project-cards", {
+      height: "10px",
+      stagger: {
+        amount: 0.4,
+      },
+      scrollTrigger: {
+        trigger: ".projects",
+        start: "top 100%",
+        end: "top -150%",
+        scrub: true,
+      },
+    });
+  });
+
   return (
-    <div className="p-3 text-black bg-white">
+    <div className="p-3 text-black bg-white mb-[100vh]">
       <div className="pt-[39.5vh]">
         <h2 className="font-[font2] uppercase text-[13vw] relative ">
           Projets{" "}
           <span className="absolute text-[2.5rem] top-14 right-[37vw]">16</span>
         </h2>
       </div>
-      <div className="-mt-20">
-        <ProjectsCard/>
+      <div className="-mt-20 projects">
+        {images.map((elem, idx) => {
+          return (
+            <div key={idx} className="project-cards w-full flex gap-4 mb-4 ">
+              <ProjectsCard image1={elem.img1} image2={elem.img2} />;
+            </div>
+          );
+        })}
       </div>
     </div>
   );
